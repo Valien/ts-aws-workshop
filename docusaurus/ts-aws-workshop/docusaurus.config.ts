34bed3f6766a5c7f0a86ed4bf969e4ee7ca3dbef
Allen Vailliencourt <chumpmonkey@gmail.com>
2025-01-16 13:23:54 -0500

    prep for ghp deploy

--- docusaurus/ts-aws-workshop/docusaurus.config.ts
+++ docusaurus/ts-aws-workshop/docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ts-aws-workshop.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ts-aws-workshop/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Valien', // Usually your GitHub org/user name.
+  projectName: 'ts-aws-workshop', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
